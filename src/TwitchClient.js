852130568fa2f069ebcd084310c7838de244cee6
Erwan HAQUET <ehaquet.dev@gmail.com>
2019-04-06 12:26:04 +0200

    TwitchClient try to reconnect if disconnetced

--- src/TwitchClient.js
+++ src/TwitchClient.js
@@ -1,6 +1,10 @@
 const tmi = require('tmi.js');
 
 const options = {
+    connection: {
+        reconnect: true,
+        maxReconnectAttempts: 5
+    },
     identity: {
         username: process.env.TWITCH_BOT_USERNAME,
         password: process.env.TWITCH_OAUTH_TOKEN
